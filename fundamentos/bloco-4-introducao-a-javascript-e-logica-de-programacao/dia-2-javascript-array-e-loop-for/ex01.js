15246d6058cfd3a98f36e05c7a5b5dcc8c8b4daa
Andre Lucas <andrelucca99@gmail.com>
2022-03-08 16:03:30 -0300

    refazendo exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex01.js
@@ -1,6 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
 
-for (let soma of numbers) {
-    soma += 1;
-    console.log(soma);
+for (let i = 0; i < numbers.length; i += 1) {
+    let numero = numbers[i];
+
+    total = total + numero;
+    console.log(total);
 }
