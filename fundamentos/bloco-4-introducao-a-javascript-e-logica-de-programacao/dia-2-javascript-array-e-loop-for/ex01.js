8a78b7bb602191829b544de03177efde8b703644
Andre Lucas <andrelucca99@gmail.com>
2022-03-08 18:11:56 -0300

    exercicio 5 resolvido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex01.js
@@ -1,18 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let total = 0;
+
+let numeroMaior = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
+
     let numero = numbers[i];
 
-    total = total + numero;
-}
+    if (numero > numeroMaior) {
+        numeroMaior = numero;
+    }
+} 
 
-let media = total / numbers.length;
-
-console.log(media);
-
-if (media > 20) {
-    console.log("valor maior que 20");
-} else {
-    console.log("valor menor ou igual a 20");
-}
+console.log(numeroMaior);
