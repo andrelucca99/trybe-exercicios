b24e0eace269c976b3b4552b04c02bff382f2da0
Andre Lucas <andrelucca99@gmail.com>
2022-03-11 08:41:26 -0300

    funcao ex 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,22 +1,10 @@
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
+function verifica(palindromo) {
+  for (index in palindromo) {
+    if (palindromo[index] !== palindromo[(palindromo.length - 1) - index]) {
+      return false
+    }
+  }
+    return true
+}
 
-  leitor.livrosFavoritos.push (
-      {
-        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-        autor: 'JK Rowling',
-        editor: 'Rocco'
-      },
-  );
-
-console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+console.log(verifica('arara'));
