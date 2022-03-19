4bdd41242417158ccc70c18f5cd984c0c2b9b46b
Andre Lucas <andrelucca99@gmail.com>
2022-03-19 09:19:23 -0300

    exer 02 muda cor da fonte

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -5,6 +5,16 @@ window.onload = function() {
     localStorage.setItem('backgroundColor', color);
   }
 
+  function corDaFont (color) {
+    let p = document.querySelectorAll('.paragrafo');
+    for (let i = 0; i < p.length; i += 1) {
+      p[i].style.color = color;
+    }
+    localStorage.setItem('fontColor', color);
+  }
+
+  // evento addEventListener
+
   let backgroundButtons = document.querySelectorAll('#background-color>button');
   for (let i = 0; i < backgroundButtons.length; i += 1) {
     backgroundButtons[i].addEventListener('click', function(event) {
@@ -12,9 +22,21 @@ window.onload = function() {
     })
   }
 
+  let fontButtons = document.querySelectorAll('#font-color > button');
+  for (let i = 0; i < fontButtons.length; i += 1) {
+    fontButtons[i].addEventListener('click', function(event) {
+      corDaFont(event.target.innerHTML);
+    })
+  }
+
+  // executa a função
+
   function executa() {
     let backgroundColor = localStorage.getItem('backgroundColor');
-    if (backgroundColor) corBackground('backgroundColor');
+    if (backgroundColor) corBackground(backgroundColor);
+
+    let fontColor = localStorage.getItem('fontSize');
+    if (fontColor) corDaFont(fontColor);
   }
   executa()
 
