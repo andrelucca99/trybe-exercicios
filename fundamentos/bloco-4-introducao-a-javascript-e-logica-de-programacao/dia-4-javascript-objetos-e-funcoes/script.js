e43baf078d6504ff88eada0c2b00cc3ad212c856
Andre Lucas <andrelucca99@gmail.com>
2022-03-24 18:09:36 -0300

    iniciando bloco 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,11 +1,21 @@
-function maiorNome(nomes) {
-  let nomeDoMaior = nomes[0];
-
-  for (let index in nomes) {
-    if (nomeDoMaior.length < nomes[index].length) {
-      nomeDoMaior = nomes[index];
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero += 1;
+      }
     }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
   }
-  return nomeDoMaior;
+  return numeros[indexNumeroRepetido];
 }
-console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
