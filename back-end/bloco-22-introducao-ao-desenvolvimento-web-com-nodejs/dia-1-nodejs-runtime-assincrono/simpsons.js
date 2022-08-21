e21a492c6743a2991705213a142bb73a8ff45f4c
Andre Lucas <andrelucca99@gmail.com>
2022-08-21 10:34:17 -0300

    exercicio 6 p5

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/simpsons.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/simpsons.js
@@ -2,10 +2,9 @@ const fs = require('fs').promises;
 
 async function addPersonagem() {
     const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
-    const simpsons = JSON.parse(fileContent);
+    const simpsonsFamily = JSON.parse(fileContent);
     
-    const familyIds = [1, 2, 3, 4];
-    const simpsonsFamily = simpsons.filter((simpson) => familyIds.includes(Number(simpson.id)));
+    simpsonsFamily.push({ id: '8', name: 'Nelson Muntz' });
 
     await fs.writeFile('./simpsons.json', JSON.stringify(simpsonsFamily));
 }
