3fc6a7407ce9f4eec6f642e8c29be28aca76d3ab
Andre Lucas <andrelucca99@gmail.com>
2022-08-21 10:16:55 -0300

    exercicio 06 p1

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/bmi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/bmi.js
@@ -18,6 +18,33 @@ const main = () => {
     const bmi = calculaBMI(weight, height);
 
     console.log(`BMI: ${bmi.toFixed(2)}`);
+
+    if (bmi < 18.5) {
+        console.log('Status: Underweight (thin)');
+        return;
+      }
+  
+      if (bmi >= 18.5 && bmi < 25) {
+        console.log('Status: Normal weight');
+        return;
+      }
+  
+      if (bmi >= 25 && bmi < 30) {
+        console.log('Status: Overweight (overweight)');
+        return;
+      }
+  
+      if (bmi >= 30 && bmi < 35) {
+        console.log('Status: Grade I obesity');
+        return;
+      }
+  
+      if (bmi >= 35 && bmi < 40) {
+        console.log('Status: Grade II obesity');
+        return;
+      }
+  
+      console.log('Status: Obesity grades III and IV');
 }
 
 main();
