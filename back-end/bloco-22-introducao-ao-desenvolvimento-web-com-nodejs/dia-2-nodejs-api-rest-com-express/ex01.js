fa1f6010cebdcfcbcc95f7df2b5131704e19e412
Andre Lucas <andrelucca99@gmail.com>
2022-08-22 15:33:54 -0300

    exercicio 02

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/ex01.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/ex01.js
@@ -3,3 +3,8 @@
 // 400 - O código de status de resposta HTTP 400 Bad Request indica que o servidor não pode ou não irá processar a requisição devido a alguma coisa que foi entendida como um erro do cliente
 
 // 422 - O codigo de resposta HTTP 422 Unprocessable Entity indica que o servidor entende o tipo de conteúdo da entidade da requisição, e a sintaxe da requisição esta correta, mas não foi possível processar as instruções presentes.
+
+
+// Para que é usado o código de status 401?
+
+// 401 - Na prática, isso significa que o site que você está tentando acessar não carregará até que você efetue login com um ID de usuário e uma senha válida.
