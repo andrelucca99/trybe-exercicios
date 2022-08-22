8e6280a884f60adcfbf479f65f1d462d39d8ab02
Andre Lucas <andrelucca99@gmail.com>
2022-08-22 15:32:16 -0300

    exercicio 01

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/ex01.js
@@ -0,0 +1,5 @@
+// 1 - Para que é usado o código de status 400 e 422?
+
+// 400 - O código de status de resposta HTTP 400 Bad Request indica que o servidor não pode ou não irá processar a requisição devido a alguma coisa que foi entendida como um erro do cliente
+
+// 422 - O codigo de resposta HTTP 422 Unprocessable Entity indica que o servidor entende o tipo de conteúdo da entidade da requisição, e a sintaxe da requisição esta correta, mas não foi possível processar as instruções presentes.
